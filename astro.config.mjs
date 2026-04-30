77519f9475e3059e80d59e826eeee54be9561453
Mauricio Pineda <142756854+mauriciop-dev@users.noreply.github.com>
2026-04-30 09:09:12 -0500

    SEO: add www canonical domain and redirects for Cloudflare Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://materiaprogramable.com',
+	site: 'https://www.materiaprogramable.com',
 	output: 'static',
 	integrations: [mdx(), sitemap()],
 });
